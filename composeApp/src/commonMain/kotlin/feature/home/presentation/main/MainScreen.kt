4a88a6bafad788c04d3fd0fa55c578f15538a768
konradjurkowski <konrad.j@thehouseofcode.com>
2024-06-08 23:14:40 +0200

    MH-1 setup MainScreen

--- composeApp/src/commonMain/kotlin/feature/home/presentation/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/feature/home/presentation/main/MainScreen.kt
@@ -1,11 +1,14 @@
 package feature.home.presentation.main
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
@@ -53,24 +56,43 @@ class MainScreen : Screen {
                     }
                 },
                 bottomBar = {
-                    NavigationBar(
-                        modifier = Modifier
-                            .shadow(
-                                elevation = Dimens.defaultElevation,
-                                ambientColor = MaterialTheme.colorScheme.onBackground,
-                                spotColor = MaterialTheme.colorScheme.onBackground,
-                            ),
-                        containerColor = MaterialTheme.colorScheme.background,
-                        tonalElevation = 0.dp,
-                    ) {
-                        val tabNavigator = LocalTabNavigator.current
+                    Box(modifier = Modifier.fillMaxWidth()) {
+                        NavigationBar(
+                            modifier = Modifier
+                                .shadow(
+                                    elevation = Dimens.defaultElevation,
+                                    ambientColor = MaterialTheme.colorScheme.onBackground,
+                                    spotColor = MaterialTheme.colorScheme.onBackground,
+                                ),
+                            containerColor = MaterialTheme.colorScheme.background,
+                            tonalElevation = 0.dp,
+                        ) {
+                            val tabNavigator = LocalTabNavigator.current
 
-                        tabList.forEach { tab ->
-                            NavigationItem(
-                                tab = tab,
-                                selected = tabNavigator.current == tab,
-                                onClick = { tabNavigator.current = tab }
-                            )
+                            tabList.subList(0, 2).forEach { tab ->
+                                NavigationItem(
+                                    tab = tab,
+                                    selected = tabNavigator.current == tab,
+                                    onClick = { tabNavigator.current = tab }
+                                )
+                            }
+
+                            Spacer(modifier = Modifier.weight(1f))
+
+                            tabList.subList(2, tabList.size).forEach { tab ->
+                                NavigationItem(
+                                    tab = tab,
+                                    selected = tabNavigator.current == tab,
+                                    onClick = { tabNavigator.current = tab }
+                                )
+                            }
+                        }
+
+                        AddFAB(
+                            modifier = Modifier
+                                .align(Alignment.TopCenter),
+                        ) {
+                            // TODO Navigate to Add Screen
                         }
                     }
                 }
